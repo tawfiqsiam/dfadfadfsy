4700fb4b465e41c51708863de121bc7a5a7015b0
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-08 03:44:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ client.on('guildMemberAdd', member => {
 	database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
 	let nww = `${client.guilds.get(member.guild.id).memberCount}`
             let manu = nww.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:')
-            client.channels.get('473640254809702400').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${manu} Colá la  que é sucesso https://www.facebook.com/discordmoon/` })
+            client.channels.get('473640254809702400').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${manu} BEM-VINDOS A GODZILLA! <a:pulo:458416272485646376> https://discord.gg/QhhAzat` })
 	})
 	
 });
@@ -194,7 +194,7 @@ client.on('guildMemberRemove', member => {
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
 	let nww = `${client.guilds.get(member.guild.id).memberCount}`
             let manu = nww.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:')
-            client.channels.get('473640254809702400').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${manu} Colá la  que é sucesso https://www.facebook.com/discordmoon/` })
+            client.channels.get('473640254809702400').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${manu}  BEM-VINDOS A GODZILLA! <a:pulo:458416272485646376> https://discord.gg/QhhAzat` })
 	})
 
 });
