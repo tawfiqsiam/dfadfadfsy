c14fe5d589ac5ccd019a94383a2423165fe98cb2
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-08 04:58:58 -0300

    Update filtro.js

--- comandos/filtro.js
+++ comandos/filtro.js
@@ -6,29 +6,43 @@ db.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate! `! Till#8514 | Natsu#7777`");
+ if ([documento.block].includes(message.author.id)) return message.reply("<:xguardian:476061993368027148> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate.");
         
 }
 
+/*const ID = "412169234492293130";
+  let users = client.guilds.get(ID).members.get(message.author.id);
+  if (!users) 
+  return message.reply("Você não está no servidor oficial do SysopCorp");
+  let role = client.guilds.get(ID).members.get(message.author.id).roles.find("id", "467669532916449290")
+  if(!role)
+  return message.channel.send('Woww! Você descobriu um recurso para parceiros. <:DiscordPartner:467546781538975754> Mais infos de como virar parceiro digite: sy!partners.');
+   */
+
 
 if (!message.member.hasPermission('ADMINISTRATOR', 'MANAGE_MESSAGES')) 
-return message.reply(':no_entry_sign: Desculpe, o comando **filtro** está disponível apenas para usuários administrativos e gerenciadores de mensagens  do server.');
+return message.reply('<:xguardian:476061993368027148> Desculpe, o comando **filtro** está disponível apenas para usuários administrativos e gerenciadores de mensagens  do server.');
 db.Guilds.findOne({"_id": message.guild.id }, function(erra, docu) {
 if (docu) {
 
-if (!suffix) return message.reply(`Você deve especificar um filtro. Filtros disponíveis: \`convites\``);
+let nha;
+
+if (!docu.convites) nha = `Ativado`;
+else nha = `Desativado`;
+
+if (!suffix) return message.reply(`<:sysalerta:469789950938841088> Você deve especificar um filtro. Filtros disponíveis: \`invites\``);
 	
 switch (suffix.split(' ')[0]){
 	
-case 'convites': {
+case 'invites': {
 
 if (!message.member.hasPermission('MANAGE_MESSAGES'))
-return message.reply('Você precisa ter no minimo permissões de Gerenciamento de Mensagens para utilizar esse filtro.');
+return message.reply('<:xguardian:476061993368027148> Você precisa ter no minimo permissões de Gerenciamento de Mensagens para utilizar esse filtro.');
 
 if (!message.member.hasPermission('ADMINISTRATOR'))
- return message.reply(':no_entry_sign: Desculpe, este comando está disponível apenas para cargos administrativos do servidor.');
+ return message.reply('<:xguardian:476061993368027148> Desculpe, este comando está disponível apenas para cargos administrativos do servidor.');
     		
     		    if (!docu) 
     			docu = {};
@@ -38,31 +52,10 @@ if (!message.member.hasPermission('ADMINISTRATOR'))
     			docu.convites = true;
     			
     			docu.save();
-    			message.channel.send('Filtro de convites definido para ' + docu.convites);
+    			message.channel.send(`OK! ${message.author}, filtro de convites: **${nha}** <:trust:447056422346424320>`);
     			
 } 
 
-case 'words': {
-
-if (!message.member.hasPermission('MANAGE_MESSAGES'))
-return message.reply('Você precisa ter no minimo permissões de Gerenciamento de Mensagens para utilizar esse filtro.');
-
-if (!message.member.hasPermission('ADMINISTRATOR'))
- return message.reply(':no_entry_sign: Desculpe, este comando está disponível apenas para cargos administrativos do servidor.');
-
- let nha = suffix.split(' ').slice(1);
-		    	if (nha.length == 0) {
-			    	if (!docu || !docu.words)
-						return message.channel.send('Este servidor não possui filtro de palavras\n**Use:** `sy!filtro words` + as palavras a serem filtradas sem vírgulas, apenas espaço!\n\nVocê pode desativar utilizando o comando `sy!disable words`.');
-		    	} else {
-			    	if (!docu) 
-	    				docu = {};
-					
-	    			docu.words = nha.filter(w => w !== ''); // filtra palavras sem nada
-	    			docu.save();
-	    			return message.channel.send('Filtro de palavras redefinido');
-		    	}
-}
 
 }} else { 
 	var server = new db.Guilds({
