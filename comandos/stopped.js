8dc6c8e2db4c1fc8e680e913567f857030486e21
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-08 08:59:03 -0300

    Update stopped.js

--- comandos/stopped.js
+++ comandos/stopped.js
@@ -7,7 +7,7 @@ db.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
  if ([documento.block].includes(message.author.id)) return message.reply("<:xguardian:476061993368027148> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate.");
         
