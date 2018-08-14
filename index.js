4a16068f0ddb5e71ea6451f43fbf0584d8d59424
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-14 17:08:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,8 @@ client.on('guildMemberAdd', member => {
     client.users.get(member.id).send(mensagem)
   }
   })
-	  
+
+/*	
 //contador manu
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
 		
@@ -188,7 +189,7 @@ let idddd  = "435957344782909440"
 let anime =  `${client.guilds.get(idddd).memberCount.toString()}`
 let spirit = anime.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
 client.channels.get('472087921072013312').edit({ topic: `<a:SysopEmojiLOGOGIF:456242315669798914> | ${spirit} Bem-vindo (a) ao AnimeSpirit seu Otaku! #3k <a:nomparty:448302844219621376>` })
-	})	
+	})	*/
 
 	//contador + setador
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
@@ -233,6 +234,7 @@ client.on('guildMemberRemove', member => {
   }
   })
 	
+/*	
 	//contador manu
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
 		
@@ -267,7 +269,7 @@ let idddd  = "435957344782909440"
 let anime =  `${client.guilds.get(idddd).memberCount.toString()}`
 let spirit = anime.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
 client.channels.get('472087921072013312').edit({ topic: `<a:SysopEmojiLOGOGIF:456242315669798914> | ${spirit} Bem-vindo (a) ao AnimeSpirit seu Otaku! #3k <a:nomparty:448302844219621376>` })
-	})	
+	})*/	
 
 //contador + setador
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
