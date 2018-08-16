9a0ba4c38aac3de5b4ad5cde69862562e7ce3884
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-15 21:02:56 -0300

    Update balance.js

--- comandos/balance.js
+++ comandos/balance.js
@@ -2,27 +2,21 @@ const Discord = require("discord.js");
 const db = require("../database.js");
 module.exports = { task(client, message, suffix) { 
     
-    if (!['244489368717230090'].includes(message.author.id)) return message.reply(`Opa, comando em fase \`BETA\` você ainda não pode usar ;(`)
-
-    
 let user = message.mentions.users.first() ? message.mentions.users.first() : message.author;
 db.Users.findOne({"_id": user.id}, function(erro, sysop) {
+if (sysop) {    
 
-database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
+db.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate! `! Till#8514 | Natsu#7777`");
+ if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate!`");
         
 }
-    
-    let verificado;
-    if (!sysop.verificado) verificado = '';
-    else verificado = `${sysop.verificado}`
-    
+})
     const embed = new Discord.RichEmbed()
     .setThumbnail(user.avatarURL)
-    embed.setDescription(`Saldo de **${user.username}** ${verificado}\n\n
+    embed.setDescription(`Saldo de **${user.username}** ${sysop.badgep2}\n\n
 \`Moedas:\`
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 <:Sycoins:469789351358889984>  ${Number(sysop.coins).toLocaleString()} sycoins  |  <a:rubine:442715023878651905> ${Number(sysop.rubys).toLocaleString()} rubys\n
@@ -36,6 +30,22 @@ database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento)
     embed.setColor('#36393E');
     message.channel.send({embed});
     
-});
+} else {
+    var pessoa = new db.Users({
+                    _id: user.id,
+                    level: 0,
+                    xp: 0,
+                    coins: 0,
+                    rubys: 0,
+                    containers: 0,
+                    goldbox: 0,
+                    emerald: 0,
+                    
+                });
+
+                pessoa.save();
+                message.reply(`use o comando novamente`);
+            }
+    
 });
 }};
