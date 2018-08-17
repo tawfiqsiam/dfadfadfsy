746e5f64410f9b675c63f842a2cd7e54b557124c
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-17 20:56:59 -0300

    Update slow.js

--- comandos/slow.js
+++ comandos/slow.js
@@ -4,16 +4,16 @@ module.exports = {
   task(client, message, suffix) {
 
       
-database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
+/*database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090'].includes(message.author.id))
+         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
                 
  if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **Sysop**! Se você acha que é um engano nos contate.");
         
-}
+}*/
 
  if (!message.member.hasPermission("MANAGE_GUILD")) 
- return message.channel.send("Opa! Parece que você não tem permissão de utilizar este comando. Tente adicionar a permissão de Gerencisamento de Servidor a você.");
+ return message.channel.send(`<:xguardian:476061993368027148> ${message.author} | Opa! Parece que você não tem permissão de utilizar este comando. Tente adicionar a permissão de Gerencisamento de Servidor a você.`);
  
  if (!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) 
  return message.reply('Desculpe, não posso fazer isso. É necessario que eu tenha permissão de Gerencisamento de mensagens.')
@@ -21,14 +21,16 @@ database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento)
 let args = suffix.split(' '); 
 let sy = message.content.split(' ');
 let amount = isNaN(Number(sy[1])) ? Number(sy[2]) : Number(sy[1]);
-let mensagem = suffix;
+let mensagem = suffix; 
+console.log(mensagem);
 let user = message.mentions.users.first() ? message.mentions.users.first() : message.author;
  if(args[0] == "help"){
-message.channel.send(`Olá ${message.author} slowmode, como usar?\n\nUse: \`sy!slow\` mais tempo para ativar o slowmode.\nUse: \`sy!slowmode\` disable para desativar o slowmode.\n\n**Exemplo:** sy!slowmode 1000`);
+message.channel.send(`Olá ${message.author} slowmode, como usar?\n\nUse: \`sy!slowmode\` mais tempo para ativar o slowmode.\nUse: \`sy!slowmode\` disable para desativar o slowmode.\n\n**Exemplo:** sy!slowmode 1000`);
 return;
     }
       
 database.Guilds.findOne({ "_id": message.guild.id}, function(erro, sysop) {
+   
     if (!sysop) {
         var server = new database.Guilds({
             _id: message.guild.id,
@@ -40,35 +42,35 @@ database.Guilds.findOne({ "_id": message.guild.id}, function(erro, sysop) {
              welcomeChannel: '',
              byeChannel: '',
              dm: '',
-             slow: 1,
+             slow: 0,
         });
         server.save();
         message.reply('<:SysopLogoEMOI:439565791357042700> Um histórico foi criado. Use o comando novamente!');
 
         } else {
-        if (!amount) {
+            if (!mensagem) {
+         // if (!amount) {
     if (!sysop.slow)
-   return message.channel.send('<:sysalerta:469789950938841088> Este servidor não possui o slowmode ativado. Use `sy!slowmode help`');
+   return message.channel.send('<:sysalerta:469789950938841088> Este servidor não possui o slowmode ativado. Use `sy!slow help`');
    else 
-   return message.channel.send(`O modo lento deste servidor está ativado num tempo de: ${sysop.slow} milisegundos. Use: sy!slowmode help para setar`);
+   return message.channel.send(`O modo lento deste servidor está ativado num tempo de: ${sysop.slow} milisegundos. Use: sy!slow help para setar`);
 
     } else {
-         
-            if (mensagem !== 'disable') {
+        if (mensagem !== 'off') { 
             sysop.slow = amount * 1000
             sysop.save();
-            return message.channel.send(`Ok ${message.author}! Você definiu o slowmode com um tempo de ${sysop.slow} milisegundos.`);
+
+            return message.channel.send(`Ok ${message.author}! Você definiu o slowmode com um tempo de ${amount} segundos.`);
         } else {
             sysop.slow = 0;
             sysop.save();
-            return message.channel.send('OK! Slowmode **Desativado!**');
+            return message.reply('OK! Slowmode **Desativado!** <:trust:436717162632052753>');
         }
     
         
     }
-            
-        }
-});
+      }
+        
 });
   
 }};
