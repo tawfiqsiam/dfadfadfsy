e36b900db9351ca94149d45a90bdec5774a3f6d3
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-18 19:23:27 -0300

    Update casar.js

--- comandos/casar.js
+++ comandos/casar.js
@@ -21,8 +21,28 @@ database.Users.findOne({"_id": message.author.id}, function(erra, documento) {
 
 documento.casou = mention.username+'#'+mention.discriminator ;
 documento.save();
-message.channel.send(`${message.author} casou com ${mention} ❤`);
+message.channel.send(`${message.author} casou com ${mention}`);
 
-}});
+} else {
+      var pessoa = new database.Users({
+                        _id: message.author.id,
+                        name: message.author.username, 
+                        discrim: "#" + message.author.discriminator,
+                        bio: "sy!bio su bio",
+                        rpup: 0,
+                        adv: 0,
+                        coins: 0,
+                        rubys: 0,
+                        containers: 0,
+                        emerald: 0,
+                        goldbox: 0,
+                        casou: 'Ninguém',
+                        profile_background: 'https://cdn.discordapp.com/attachments/442346665052209153/469407730797641738/SysopLogoLight.png',
+                      
+                    })
+                    pessoa.save()
+        message.reply("<:LikeSysop3:476062023629799424> **Perfil criado!** Use novamente este comando!");
+}
+});
 });
 }};
