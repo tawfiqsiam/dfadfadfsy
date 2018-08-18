3c0a7fd189bf81f277707309d1051f9a2b503dfd
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-18 17:27:29 -0300

    Update banday.js

--- comandos/banday.js
+++ comandos/banday.js
@@ -13,21 +13,18 @@ database.Bloqueio.findOne({
                 if(documento) {
          if (!['244489368717230090','282504900552949760'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> You are blocked to use Sysop commands, if you think is this wrong contract us! `SHAZAM!#0001 | Natsu#0001`");
+ if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate! `! Till#8514 | Natsu#7777`");
         
 }
-
-if (!message.member.hasPermission('BAN_MEMBERS')) 
-return message.reply('<:xguardian:476061993368027148> Desculpe, você não tem permissão de banir usuário neste servidor!');
-
       
             let args = suffix.split(' '); 
-            let motivo =  args.slice(1).join(' ') 
+            let sysop =  args.slice(1).join(' ') 
             ? args.slice(1).join(' ')
             :  "Foi banido! Motivo não especificado.";
 
 
 
+if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('<:xguardian:476061993368027148> Desculpe, você não tem permissão de banir usuário neste servidor!');
 let id = message.mentions.users.first()
 
     ? message.mentions.users.first().id 
@@ -35,19 +32,19 @@ let id = message.mentions.users.first()
     : args
     ? args[0]
     : null;
-if (!id) return message.reply("<:sysalerta:469789950938841088> Não encontrei nenhum usuário.");
+if (!id) return message.reply('<:sysalerta:469789950938841088> Mencione alguém ou especifique um ID.');
 
 let banPerms = message.guild.member(client.user).hasPermission('BAN_MEMBERS')
-if (!banPerms)  return message.reply("<:xguardian:476061993368027148> Eu não tenho permissão para banir usuários neste servidor.");
+if (!banPerms)  return message.reply("Eu não tenho permissão para banir usuários nesse servidor.");
 
 let user = client.users.has(id) ? client.users.get(id) : null;
 
-if (!user) return message.reply("<:sysalerta:469789950938841088> Não encontrei nenhum usuário");
+if (!user) return message.reply("<:xguardian:476061993368027148> Não encontrei nenhum usuário.")
 let bannable = message.guild.member(id)
 if (bannable) {  
     if (bannable.highestRole.position >= message.member.highestRole.position) return message.reply("<:xguardian:476061993368027148> Você não pode banir este usuário pois seu cargo é menor ou igual a o do usuário a ser banido!")
     
-    if (!message.guild.member(user).bannable) return message.reply('<:sysalerta:469789950938841088> Eu não posso banir esse usuário.');
+    if (!message.guild.member(user).bannable) return message.reply('<:xguardian:476061993368027148> Eu não posso banir esse usuário.');
     
 } else {
 
