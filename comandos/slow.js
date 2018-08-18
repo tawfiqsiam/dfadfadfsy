37f89ec7dab4bab09090865d616d7905a4fa45f5
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-17 21:01:00 -0300

    Update slow.js

--- comandos/slow.js
+++ comandos/slow.js
@@ -4,13 +4,13 @@ module.exports = {
   task(client, message, suffix) {
 
       
-/*database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
+database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090''].includes(message.author.id))
                 
  if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **Sysop**! Se você acha que é um engano nos contate.");
         
-}*/
+}
 
  if (!message.member.hasPermission("MANAGE_GUILD")) 
  return message.channel.send(`<:xguardian:476061993368027148> ${message.author} | Opa! Parece que você não tem permissão de utilizar este comando. Tente adicionar a permissão de Gerencisamento de Servidor a você.`);
@@ -72,5 +72,5 @@ database.Guilds.findOne({ "_id": message.guild.id}, function(erro, sysop) {
       }
         
 });
-  
+});
 }};
