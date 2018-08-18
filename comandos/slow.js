f0853d5d878cbe910b0a8050dfe29dd0445c5c56
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-17 21:01:11 -0300

    Update slow.js

--- comandos/slow.js
+++ comandos/slow.js
@@ -6,7 +6,7 @@ module.exports = {
       
 database.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090''].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
  if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **Sysop**! Se você acha que é um engano nos contate.");
         
