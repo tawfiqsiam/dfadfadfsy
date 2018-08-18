bd902867d39e4a3c63439c425d1c51a292d77e26
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-18 05:54:24 -0300

    Update profile.js

--- comandos/profile.js
+++ comandos/profile.js
@@ -44,7 +44,7 @@ database.Bloqueio.findOne({
                  Jimp.read("./Emblemas/Level" + documento.level + ".png").then(function(emblema) {
                    // Jimp.read(user.username).then(function(user) {
                     Jimp.read(`${user.avatarURL}`).then(function(avatar) {
-                      Jimp.read("https://cdn.discordapp.com/attachments/442346561289060352/479834104875974658/PerfilSys.png").then(function(perfil) {
+                      Jimp.read("https://cdn.discordapp.com/attachments/393818943246172164/480292451009953792/PerfilSysop.png").then(function(perfil) {
                         Jimp.read(`./border/${documento.borderp}.png`).then(function(marco) {
                          
                               Jimp.read(`${imgcolor[user.presence.status]}`).then(function(status) {
@@ -58,8 +58,8 @@ database.Bloqueio.findOne({
                             avatar.resize(276, 265);
                             mascara.resize(276, 265);
                             avatar.mask(mascara, 0, 0);
-                            background.resize(770, 550);
-                            emblema.resize(50, 54);
+                            background.resize(770, 480);
+                            emblema.resize(40, 44);
                         
                             status.resize(55, 55);
                         //  pau.resize(9,490)
@@ -68,7 +68,7 @@ database.Bloqueio.findOne({
                             img.composite(perfil, 0, 0);
                             img.composite(avatar, 1, 250);
                             img.composite(marco, 1, 250);
-                            img.composite(emblema, 120, 53);
+                            img.composite(emblema, 120, 58);
                             img.composite(serverIcon, 29, 18);
                             img.composite(status, 225, 410);
                          // img.print(user, 415, 303);
@@ -76,6 +76,8 @@ database.Bloqueio.findOne({
                             img.print(letra, 285, 392, `${user.username}`);
                             img.print(letra2, 78, 29, `${server.name}`);
                             img.print(letra, 690, 392, `${user.discriminator}`);
+                            img.print(letra2, 690, 227, `${documento.rpup}`);
+                            img.print(letra2, 660, 360, `Total de: ${documento.adv}`);
                             img.print(letra, 690, 445, `${documento.level}`);
                             img.print(letra2, 675, 485, `${Number(documento.xp).toLocaleString()}`);
                         img.print(letra2, 470, 480, `${Number(documento.emerald).toLocaleString()}`);
@@ -86,7 +88,7 @@ database.Bloqueio.findOne({
                             img.print(letra2, 22, 545, `\`\`${documento.bio}\`\``, 725);
                             //img.print(letr, 200, 23, documento.xp + "/" +  Math.ceil(pudim * 400) + " (" + parseInt(documento.xp/(pudim *400 )*100) + "%)")  
                             img.getBuffer(Jimp.MIME_PNG, (erri, buffer) => {
-                              message.channel.send(``, new Discord.Attachment(buffer, 'PerfilSys.png'));
+                              message.channel.send(``, new Discord.Attachment(buffer, 'PerfilSysop.png'));
                             });
                           });
                         });
@@ -107,7 +109,26 @@ database.Bloqueio.findOne({
           message.channel.send('**Você está usando um plano de fundo inválido**');
         }
       } else {
-        message.reply("**Você não tem um perfil.** :confused: Use: `sy!register`");
+          
+            var pessoa = new database.Users({
+                        _id: message.author.id,
+                        name: message.author.username, 
+                        discrim: "#" + message.author.discriminator,
+                        bio: "sy!bio su bio",
+                        rpup: 0,
+                        adv: 0,
+                        coins: 0,
+                        rubys: 0,
+                        containers: 0,
+                        emerald: 0,
+                        goldbox: 0,
+                        nv: 0,
+                        nvxp: 0,
+                        profile_background: 'https://cdn.discordapp.com/attachments/442346665052209153/469407730797641738/SysopLogoLight.png',
+                      
+                    })
+                    pessoa.save()
+        message.reply("<:LikeSysop3:476062023629799424> **Perfil criado!** Use novamente este comando!");
       }
     });
   })
