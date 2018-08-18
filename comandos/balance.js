85bf95b1b3442b257f1b6ce19ef6be4a9c590b05
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-18 19:46:33 -0300

    Update balance.js

--- comandos/balance.js
+++ comandos/balance.js
@@ -14,9 +14,14 @@ db.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
         
 }
 })
+    let oficial;
+    
+    if (!sysop.badgep2) oficial = '';
+    else oficial = `${sysop.badgep2}`;
+    
     const embed = new Discord.RichEmbed()
     .setThumbnail(user.avatarURL)
-    embed.setDescription(`Saldo de **${user.username}** ${sysop.badgep2}\n\n
+    embed.setDescription(`Saldo de **${user.username}** ${oficial}\n\n
 \`Moedas:\`
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 <:Sycoins:469789351358889984>  ${Number(sysop.coins).toLocaleString()} sycoins  |  <a:rubine:442715023878651905> ${Number(sysop.rubys).toLocaleString()} rubys\n
