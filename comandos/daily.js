3b77f54ab339b289c61a56c2cabf65a8a3e84d31
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-19 19:09:06 -0300

    Update daily.js

--- comandos/daily.js
+++ comandos/daily.js
@@ -1,10 +1,13 @@
- let ms = require("parse-ms");  
+      let ms = require("parse-ms");  
         let database = require("../database.js")
         var dayCol = []
         const Discord = require("discord.js");
         const db = require('quick.db');
  
- module.exports = { task(client, message, suffix) {
+ module.exports = {
+    categoria: 'Economia',
+    description: 'Receba seu bônus diário!',
+    task(client, message, suffix) {
             
 database.Bloqueio.findOne({
                 "_id": message.author.id
@@ -12,34 +15,12 @@ database.Bloqueio.findOne({
                 if(documento) {
          if (!['244489368717230090','282504900552949760'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate!");
+ if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate! `! Till#8514 | Natsu#7777`");
         
 }
    
- if (!['244489368717230090'].includes(message.author.id)) return message.reply('desculpe o transtorno, este comando encontra-se em manutenção, tente novamente mais tarde.');
+  /*if (!['244489368717230090'].includes(message.author.id)) return message.channel.send('Comando em manutenção!');*/
         
- database.Users.findOne({
-            "_id": message.author.id
-        }, function(erro, documento) {
-            
-            if (!documento) {
-                var pessoa = new database.Users({
-                        _id: message.author.id,
-                        name: message.author.username, 
-                        discrim: "#" + message.author.discriminator,
-                        bio: "Sobre você",
-                        marry: "None",
-                        nexDay: 0,
-                        day: 0,
-                        temprep: 0,                       
-                      
-                    })
-                    pessoa.save()
-                     message.reply("Registro realizado com sucesso! Utilize o comando novamente!")
-                
-            } else {
- 
-
        if(!dayCol.includes(message.author.id)) {
             
             var prc = Math.round(Math.random() * 80);
@@ -48,14 +29,8 @@ database.Bloqueio.findOne({
             "_id": message.author.id
         }, function (erro, documento) {
             if(documento) {
-                
-const moment = require('moment');
-require("moment-duration-format");
-moment.locale('pt-BR');             
                  
- var tempo = moment.duration.format([moment.duration((parseInt(documento.day) + 86400000) - Date.now())], "hh:mm:ss");
-                 
-       if ((parseInt(documento.day) + 86400000) <= (Date.now())) {       
+              
                 var premio;
                 var quantidade;
                 
@@ -64,48 +39,42 @@ moment.locale('pt-BR');
                     quantidade = 5;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.containers += dayRDM
-                    documento.day = Date.now();
                     documento.save()
                 } else  if (prc <= 40) {
                     premio = `rubys <:rubys:430493599633899532>`;
                     quantidade = 500 + 2000;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.rubys += dayRDM
-                    documento.day = Date.now();
                     documento.save()
                 } else if (prc <= 80) {
-                    premio = `Sycoins <:Sycoins:469789351358889984>`;
+                    premio = `coins <:Sycoins:469789351358889984>`;
                     quantidade = 500 + 3000;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.coins += dayRDM
-                    documento.day = Date.now();
                     documento.save()
             } else if (prc <= 40) {
-                    premio = `goldboxs <:New_goldenbox_10:458877483182194698>`;
+                    premio = `goldboxs <:New_goldenbox_10:458877483182194698>;
                     quantidade = 5 + 15;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.goldbox += dayRDM
-                    documento.day = Date.now();
                     documento.save()
             } else if (prc <= 40) {
                     premio = `XP's <:New_goldenbox_10:458877483182194698>`;
+                    quantidade = 5 + 2000;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.xp += dayRDM
-                    documento.day = Date.now();
                     documento.save()
             } else if (prc <= 60) {
                     premio = `lootbox <:ContainerEmoji:436348542576230411>`;
                     quantidade = 1 + 3;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.lootbox += dayRDM
-                    documento.day = Date.now();
                     documento.save()
-            } else if (prc <= 40) {
+                } else if (prc <= 20) {
                     premio = `esmeraldas <a:esmeral:442718169585680387>`;
-                    quantidade = 30;
+                    quantidade = 10;
                     var dayRDM = Math.round(Math.random() * parseInt(quantidade) + 1)
                     documento.emerald += dayRDM
-                    documento.day = Date.now();
                     documento.save()
                 }
                 
@@ -117,15 +86,22 @@ moment.locale('pt-BR');
                        embed.setThumbnail('https://cdn.discordapp.com/attachments/442346665052209153/469407730797641738/SysopLogoLight.png')
                        embed.setAuthor(message.author.username, message.author.avatarURL)
                        embed.addField(`**Bônus Diário!**`,` <a:festa:461509706763206657> Você ganhou: **${dayRDM} ${premio}** .`, false)
-                       .setColor(0x0df7eb);
+                       .setColor('#36393E');
    
     value.edit(embed)}, 3000)
   
  
-}) } else {
+})
 
-message.channel.send(`<a:pblob:442467218282774548> Opa ${message.author}! Colete seu bônus diário novamente em \`${tempo}\``)
-}
+
+              dayCol.push(message.author.id)
+              setTimeout(function () {
+                    for(var i = 0; i < dayCol.length; i++) {
+                        if(dayCol[i] === message.author.id) {
+                            dayCol.splice(i, 1);
+                        }
+                    }
+                }, 5 * 1000 * 60 * 60);
 
             } else {
 
@@ -137,7 +113,6 @@ message.channel.send(`<a:pblob:442467218282774548> Opa ${message.author}! Colete
                     rubys: 0,
                     containers: 0,
                     nextDay: 0,
-                    temprep: 0,
                     })
 
                 pessoa.save()
@@ -146,13 +121,11 @@ message.channel.send(`<a:pblob:442467218282774548> Opa ${message.author}! Colete
 
             
         }).catch(e => console.log(e));
-   
-                }
-            }
-   
-})
+        
+   } else {
+       message.channel.send(`<:sysalerta:471836304938631168> Woww ${message.author}, você já coletou seu bônus diário! Volte novamente em 5 horas. `);
+    }
 })
         }
         
 };
-
