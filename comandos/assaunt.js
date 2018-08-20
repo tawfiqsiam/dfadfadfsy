75d29f2387025b38fcfe0fafaf43897b7538159b
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-20 12:14:00 -0300

    Update assaunt.js

--- comandos/assaunt.js
+++ comandos/assaunt.js
@@ -56,7 +56,7 @@ database.Bloqueio.findOne({
                         } else {
                             documento.coins -= dayRDM
                             documento.save();
-                            message.reply("<a:pm:436911890770165760> **Ih Serjão sujo! Você foi pego ao tentar roubar e perdeu " + Number(dayRDM).toLocaleString() + " coins!**");
+                            message.channel.send(`<a:pm:436911890770165760> Ih ${message.author} sujo! Você foi pego ao tentar <@${message.mentions.users.first().id} roubar e perdeu ${Number(dayRDM).toLocaleString()} Sycoins!");
                             dayCol.add(message.author.id)
                             setTimeout(function() {
                                 dayCol.delete(message.author.id)
