58c6c66ea51ba849cd76fe6bf8d00fd4030f4e08
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-23 18:44:24 -0300

    Create level.js

---
+++ comandos/level.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+module.exports = { task(client, message, suffix) {
+    
+const db = require("../database.js");
+db.Bloqueio.findOne({
+                "_id": message.author.id
+            }, function (erro, documento) {
+                if(documento) {
+         if (!['244489368717230090'].includes(message.author.id))
+                
+ if ([documento.block].includes(message.author.id)) return message.reply("<:xguardian:476061993368027148> Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate!");
+        
+}
+
+db.Guilds.findOne({"_id": message.guild.id}, function(erro, sysop) {
+
+if (sysop) {
+
+switch (suffix.split(' ')[0]){
+	
+case 'on': {
+
+sysop.upa  = true ;
+sysop.save();
+message.channel.send(`:white_check_mark: | Ok ${message.author} você ativou o sistema de levels.`) ;
+}
+case 'off': {
+
+sysop.upa  = false ;
+sysop.save();
+message.channel.send(`:no_entry_sign: | Ok ${message.author} você desativou o sistema de levels.`) ;
+}}} else {
+    var server = new db.Guilds({
+            _id: message.guild.id,
+             convites: false,
+             sugestao: '',
+             welcome: '',
+             words: [],
+             autorole: '',
+             welcomeChannel: '',
+             byeChannel: '',
+             dm: '',
+             nivel: 0,
+             exps: 0,
+             upa: true,
+        });
+        server.save();
+        message.reply('<:SysopLogoEMOI:439565791357042700> Um histórico foi criado. Use o comando novamente!');
+}
+});    
+});
+}};
