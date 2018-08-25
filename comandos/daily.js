946510fb99ad97106f80635adc5cb0bc28d9fe82
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-25 20:26:57 -0300

    Update daily.js

--- comandos/daily.js
+++ comandos/daily.js
@@ -19,7 +19,7 @@ database.Bloqueio.findOne({
         
 }
    
-  if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, técnicos estão realizando uma manutenção atualmente. Tente novamente este comando mais tarde.`);
+ // if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, técnicos estão realizando uma manutenção atualmente. Tente novamente este comando mais tarde.`);
         
        if(!dayCol.includes(message.author.id)) {
             
