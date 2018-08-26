88dc3c04dda3c1636a551df58ff48c28e2dd053d
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-26 15:00:49 -0300

    Update container.js

--- comandos/container.js
+++ comandos/container.js
@@ -17,7 +17,7 @@ database.Bloqueio.findOne({
         
 }
 	    
- if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, comando em manutenção!`);
+// if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, comando em manutenção!`);
 	
         suffix = suffix.split(' '); 
         let razaou = suffix[0];
