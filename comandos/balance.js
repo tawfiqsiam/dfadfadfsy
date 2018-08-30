212004ffa3450856ee8f2e140a4e0ab072ad6dec
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-30 07:30:45 -0300

    Update balance.js

--- comandos/balance.js
+++ comandos/balance.js
@@ -20,7 +20,7 @@ database.Bloqueio.findOne({
         
 }
   
-   if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, comando em manutenção!`);
+   //if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, comando em manutenção!`);
 
 
     let user = message.mentions.users.first() ? message.mentions.users.first() : message.author;
