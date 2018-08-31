a85747797a069611b1583a0761a4081eb2f41d33
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-31 02:00:45 -0300

    Update by.js

--- comandos/by.js
+++ comandos/by.js
@@ -27,117 +27,117 @@ database.Bloqueio.findOne({
             if(documento) {
                 
                 if (razaou === 'setbackground') {
-                    if(Number(documento.rubys) >= 35650) {
-                        documento.rubys = Number(documento.rubys) - 35650
+                    if(Number(documento.rubys) >= 650000) {
+                        documento.rubys = Number(documento.rubys) - 650000
                         documento.background_shoped = "yes"
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author}, Compra realizada com sucesso!`)
                         documento.save()
                     } else {
-                        message.channel.send('Você precisa de 20.000 rubys...')
+                        message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.atuhor}! Você precisa de 650.000 Sy Crystal <:sysopcrystal:482651147111366656>`)
                     }
                 } else if(razaou === 'bio') {
-                    if(Number(documento.emerald) >= 200) {
-                        documento.emerald = Number(documento.emerald) - 200
+                    if(Number(documento.emerald) >= 10000) {
+                        documento.emerald = Number(documento.emerald) - 10000
                         documento.bio_shoped = "yes"
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author}, Compra realizada com sucesso!`)
                         documento.save()
                     } else {
-                        message.channel.send('Você precisa de 200 esmeraldas <a:esmeral:442718169585680387>')
+                        message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}! Você precisa de 10.000 esmeraldas <a:esmeral:442718169585680387>`)
                     }
                     
                 } else if(razaou === 'goldbox') {
-                    if(Number(documento.rubys) >= 20900) {
-                        documento.rubys = Number(documento.rubys) - 20900
+                    if(Number(documento.rubys) >= 40900) {
+                        documento.rubys = Number(documento.rubys) - 40900
                         documento.goldbox_shoped = "yes"
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                     } else {
-                        message.channel.send('Você precisa de 20.900 rubys...')
+                        message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}! Você precisa de 40.900 Sy Crsytal`)
                     }
                 } else if(razaou === 'setborder') {
-                    if(Number(documento.rubys) >= 10000) {
-                        documento.rubys = Number(documento.rubys) - 10000
+                    if(Number(documento.rubys) >= 100000) {
+                        documento.rubys = Number(documento.rubys) - 100000
                         documento.border_shoped = "yes"
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 10.000 rubys...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author}! Você precisa de 100.000 Sy Crystal.`)
                     }
                 } else if(razaou === 'Nv1') {
                     if(Number(documento.emerald) >= 100) {
                         documento.emerald = Number(documento.emerald) - 100
                         documento.coins = Number(documento.coins) + 5000
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 100 esmeraldas')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author} Você precisa de 100 esmeraldas.`)
                     }
                 } else if(razaou === 'Nv2') {
                     if(Number(documento.emerald) >= 1000) {
                         documento.emerald = Number(documento.emerald) - 1000
                         documento.coins = Number(documento.coins) + 15000
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 1.000 esmeraldas...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author}! Você precisa de 1.000 esmeraldas.`)
                     }
                 } else if(razaou === 'Nv3') {
                     if(Number(documento.rubys) >= 1000) {
                         documento.rubys = Number(documento.rubys) - 1000
                         documento.coins = Number(documento.coins) + 5000
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                       message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 1.000 Sy Crystal...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author} Você precisa de 1.000 Sy Crystal..`)
                     }
                 } else if(razaou === 'Nv4') {
                     if(Number(documento.rubys) >= 10000) {
                         documento.rubys = Number(documento.rubys) - 10000
                         documento.coins = Number(documento.coins) + 40000
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 10.000 Sy Crystal...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author}! Você precisa de 10.000 Sy Crystal.`)
                     }
                 } else if(razaou === 'Nv5') {
                     if(Number(documento.coins) >= 100000) {
                         documento.rubys = Number(documento.coins) - 100000
                         documento.emerald = Number(documento.emerald) + 500
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 100.000 Sycoins...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author}! Você precisa de 100.000 Sycoins.`)
                     }
                 } else if(razaou === 'Nv6') {
                     if(Number(documento.containers) >= 5) {
                         documento.containers = Number(documento.containers) - 5
                         documento.goldbox = Number(documento.goldbox) + 35
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                       message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 5 containers...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author}! Você precisa de 5 containers.`)
                     }
                 } else if(razaou === 'Nv7') {
                     if(Number(documento.goldbox) >= 100) {
                         documento.goldbox = Number(documento.goldbox) - 100
                         documento.containers = Number(documento.containers) + 20
-                        message.channel.send('<a:anirainbowblob:434952813177470986> Compra realizada com sucesso!')
+                        message.channel.send(`<a:anirainbowblob:434952813177470986> ${message.author} Compra realizada com sucesso!`)
                         documento.save()
                         
                     } else {
-                        message.channel.send('Você precisa de 100 Goldbox...')
+                        message.channel.send(`<:sysopcrystal:482651147111366656> Opa ${message.author}! Você precisa de 100 Goldbox.`)
                     }
                
                
                 } else {
-  message.channel.send(`<:sysalerta:469789950938841088>${message.author} especifique algo para comprar! **Use** \`sy!shop\``)
+  message.channel.send(`<:sysalerta:469789950938841088> ${message.author} especifique algo para comprar! **Use** \`sy!shop\``)
                 }
                 
             } else {
