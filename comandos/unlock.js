b97d8183cffa13b5ffc1cf974bd7126dbbbfe2ff
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 01:17:50 -0300

    Update unlock.js

--- comandos/unlock.js
+++ comandos/unlock.js
@@ -10,9 +10,10 @@ db.Bloqueio.findOne({
                 if(documento) {
          if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:xguardian:476061993368027148> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate.");
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
         
 }
+  
  if (!message.member.hasPermission("BAN_MEMBERS")) 
  return message.channel.send("<:xguardian:476061993368027148> Opa! Parece que você não tem permissão de utilizar este comando. Tente adicionar a permissão de Banir a você.");
  
