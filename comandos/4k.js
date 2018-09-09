422308f93f6277a5aae0371e39c2f9abb428f5e2
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 00:35:32 -0300

    Update 4k.js

--- comandos/4k.js
+++ comandos/4k.js
@@ -12,7 +12,7 @@ database.Bloqueio.findOne({
                 if(documento) {
          if (!['244489368717230090','282504900552949760'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate! `! Till#8514 | Natsu#7777`");
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
         
 }
         
