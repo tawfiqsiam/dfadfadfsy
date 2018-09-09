c35217dbb8867a76093bf040e029fa7e0a3428d8
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 00:42:35 -0300

    Update casar.js

--- comandos/casar.js
+++ comandos/casar.js
@@ -6,9 +6,9 @@ database.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> You are blocked to use Sysop commands, if you think is this wrong contract us! `SHAZAM!#0001 | Natsu#0001`");
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
         
 }
 
