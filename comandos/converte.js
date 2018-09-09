a79a5eda3ca6d15e4386083420ef8e45ce9f9718
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 00:46:30 -0300

    Update converte.js

--- comandos/converte.js
+++ comandos/converte.js
@@ -1,5 +1,15 @@
 module.exports = { task(client, message, suffix) {
  
+ database.Bloqueio.findOne({
+                "_id": message.author.id
+            }, function (erro, documento) {
+                if(documento) {
+         if (!['244489368717230090'].includes(message.author.id))
+                
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
+        
+}
+ 
 if (!suffix)
 return message.channel.send(`<:sysalerta:469789950938841088> ${message.author} defina o texto que você gostaria de converter!`);
     
@@ -17,4 +27,5 @@ suffix.replace(/A/gi, 'ᴀ')
    
     
 message.channel.send(`Texto de \`${user.username}\`\n\n\`\`\`http\n${fonte}\`\`\``);
+ });
 }};
