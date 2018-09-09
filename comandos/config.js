f617556d788e0f995b90f0390bda96426668e527
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 00:44:45 -0300

    Update config.js

--- comandos/config.js
+++ comandos/config.js
@@ -10,7 +10,7 @@ database.Bloqueio.findOne({
                 if(documento) {
          if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate.");
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
         
 }
    
