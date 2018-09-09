5329ca89d9885e1f1b0bfe32bc18fabf485421ca
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 01:19:24 -0300

    Update assaunt.js

--- comandos/assaunt.js
+++ comandos/assaunt.js
@@ -18,8 +18,8 @@ database.Bloqueio.findOne({
 }
         
        
-      if (['244489368717230090', '481829760369033228','345014174004543488', '431334258653528066'].includes(message.mentions.users.first().id))
-      return message.channel.send(`<:sysalerta:469789950938841088> | Opa ${message.author} você não pode roubar este usuário.`)    
+    /*  if (['244489368717230090', '481829760369033228','345014174004543488', '431334258653528066'].includes(message.mentions.users.first().id))
+      return message.channel.send(`<:sysalerta:469789950938841088> | Opa ${message.author} você não pode roubar este usuário.`)   */ 
             
     
     let user = message.mentions.users.first();
