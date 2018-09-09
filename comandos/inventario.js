05bef2199edbdc56494ddbe3e52f50e649cf3bdd
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 00:57:16 -0300

    Delete inventario.js

--- comandos/inventario.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    task(client, message, suffix) {
-    
- message.reply(`Wow! Parece que existe um novo comando. Tente por \`sy!story\` e \`sy!balance\`\nCaso tenha dúvidas user **sy!help** para entrar em meu servidor Oficial!`);
-    
-    }};
