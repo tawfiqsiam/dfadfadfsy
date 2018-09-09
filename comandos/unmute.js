463216c89fccec38da74148b318e37e7755ad52b
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 01:18:06 -0300

    Update unmute.js

--- comandos/unmute.js
+++ comandos/unmute.js
@@ -4,9 +4,9 @@ db.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:xguardian:476061993368027148> Você foi bloqueado de usar comandos do **Sysop**. Se acha que é um engano nos contate!`");
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
         
 }
 
