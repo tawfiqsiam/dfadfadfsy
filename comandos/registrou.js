62350d4e99184825e775ffc981984cf1e41e5a52
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 01:09:44 -0300

    Update registrou.js

--- comandos/registrou.js
+++ comandos/registrou.js
@@ -5,7 +5,15 @@ var dayCol = []
 
 module.exports = {
     task(client, message, suffix) {
-
+database.Bloqueio.findOne({
+                "_id": message.author.id
+            }, function (erro, documento) {
+                if(documento) {
+         if (!['244489368717230090'].includes(message.author.id))
+                
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
+        
+}
 
 
   database.Guilds.findOne({"_id":message.guild.id},function(erro,documento){
@@ -85,5 +93,5 @@ message.channel.send({embed});
 
 }
               })
-        
+})
      }};
