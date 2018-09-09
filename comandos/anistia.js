98470f7f20c1090b543fc9b0de14cb9fb334c461
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-09 00:36:57 -0300

    Update anistia.js

--- comandos/anistia.js
+++ comandos/anistia.js
@@ -7,15 +7,15 @@ module.exports = {
      message.delete();
  
 let database = require("../database.js");  
-/*database.Bloqueio.findOne({
+database.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090','282504900552949760'].includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **SysopCorp**, se você acha que isso é um engano nos contate! `! Till#8514 | Natsu#7777`");
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> | ${message.author}! Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate! `);
         
-}*/
+}
       
 if (!message.member.hasPermission('BAN_MEMBERS')) 
 return message.reply('<:FalseSysop3:462306755150479372> Desculpe, você não tem permissão de banir usuário neste servidor!');
@@ -79,6 +79,6 @@ if (user) {
     message.reply('<:FalseSysop3:462306755150479372> Não encontrei nenhum usuário. Talvez ele não esteja em nenhum servidor compartilhado comigo.');
 
 }
-    
+});
 }
 };
