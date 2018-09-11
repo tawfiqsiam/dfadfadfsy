bed9fd69050b739665ba45511e8104ea97adc854
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-10 21:16:22 -0300

    Update batalhar.js

--- comandos/batalhar.js
+++ comandos/batalhar.js
@@ -1,5 +1,4 @@
 const db = require("../database.js");
-var dayCol = new Set()
 
 module.exports = { task(client, message, suffix) {
 
@@ -16,21 +15,27 @@ db.Bloqueio.findOne({
 var nire = message.mentions.users.first();
 var qnire = message.author;
 
+db.Users.findOne({
+                "_id": message.author.id
+            }, function (erro, documento) {
+                if (documento) {
+                    if(message.author.bot) return;
+               
+                    const HOUR =  60 * 60 * 1E3;
+                    let current = documento.batalhar;
+                    if (current == 0)
+                    current = Date.now() - 2 * HOUR; // primeiro trabalho
+                    console.log(current);
 
-if (dayCol.has(message.author.id)) 
-return message.channel.send(`<:sysalerta:469789950938841088> **|** ${message.author} batalhe novamente dentro de 5 horas.`)
-
+                    if (new Date() >= current) {
+                    documento.batalhar = Date.now() + 2 * HOUR;
+                    documento.save();
 
 if (!nire) 
 return message.channel.send(`<:sysalerta:469789950938841088> ${message.author} mencione alguém para batalhar.`);
 
-if (message.mentions.users.first().id == message.author.id) 
-return message.channel.send(`<:xguardian:476061993368027148> | Opa ${message.author}! Você não pode batalhar com você mesmo.`);
 
-if (message.mentions.users.first().bot) 
-return message.channel.send(`<:xguardian:476061993368027148> | ${message.author} estranho... Você não pode batalhar com um bot.`);
-
-message.channel.send(`<a:PulandoFeliz:486648350888689666> **|** ${qnire} te desafiou para uma batalha <@${nire.id}>. Você aceita o desafio? Responda: \`" sim " ou " não "\``).then(() => {
+message.channel.send(`<a:PulandoFeliz:486648350888689666> **|** ${qnire} te desafiou para uma batalha <@${nire.id}>. Você aceita o desafio?\nResponda: \`sim ou não\``).then(() => {
 message.channel.awaitMessages(res => 
 (res.content == "sim" && res.author.id == nire.id) || (res.content == 'não' && res.author.id == nire.id), { 
 
@@ -65,7 +70,8 @@ max: 1, time: 60000, errors: ['time'] }).then(col => {
                 
                    if (documento.coins > 0) {
                   
-                let wonCoins = 10000 
+                let premio = 15000;
+                               
                 
                                 db.Users.findOne({ 
                                 "_id": ganhador,
@@ -73,25 +79,59 @@ max: 1, time: 60000, errors: ['time'] }).then(col => {
                                 if (!doc) {
                                     doc = createAccount(ganhador.user.id);
                                 }
-                                doc.coins += wonCoins;
+                                doc.coins += premio;
                                 doc.save();
                             });
-message.channel.send(`:bomb: Batalhando...`).then(function(value) {
-value.edit(`<@${ganhador}> venceu a batalha e recebeu um prêmio de: **${Number(wonCoins).toLocaleString()}** Sycoins <:Sycoins:469789351358889984>`)}, 30000)
-                  
-        }
-            }})
+            message.channel.send(`:bomb: Batalhando...`).then(function(value) {
+
+                         value.edit(`<@${ganhador}> venceu a batalha e recebeu um prêmio de: **15.000** Sycoins Sycoins <:Sycoins:469789351358889984>`
+                   
+                , 5000)
+  
+ 
+})
+            }
+            }});
+    }
     if (col.first().content == 'não') {
         message.channel.send(`:crossed_swords: **|** ${nire} recusou o desafio.`);
     }
-      dayCol.add(message.author.id)
-                        setTimeout(function() {
-                            dayCol.delete(message.author.id)
-                        }, 5 * 1000 * 60 * 60)
-    }
-   
-}).catch(() => message.channel.send(`:shrug::skin-tone-2: **|** ${qnire} Passou-se **1** minuto sem resposta. Sua batalha foi finalizada a força.`));
+}).catch(() => message.channel.send(`:shrug::skin-tone-2: **|** Passou-se **1** minuto sem resposta. Batalha finalizada a força.`));
+
     
-});
+    
+});  } else {
+    
+            let restante = current - Date.now();
+                let humanize = require('humanize-duration');
+                let humanize_config = {
+                 language: 'pt',
+                 conjunction: ' e ',
+                 serialComma: false,
+                 round: true,
+                 units: ['h', 'm', 's']
+    };
+    
+    message.channel.send(`<a:swbouce:488754110175379456> **│** ${message.author}! Você precisa esperar **${humanize(restante, humanize_config)}** para batalhar novamente.`);
+
+    
+}
+    
+  } else {
+        let pessoa = new db.Users({
+                        _id: message.author.id,
+                        level: 0,
+                        xp: 0,
+                        coins: 0,
+                        rubys: 0,
+                        containers: 0,
+                        nextDay: 0,
+                        daily: 0,
+                        batalhar: 0,
+                    });
+                    pessoa.save();
+                
+        
+}}) 
 });
 }};
